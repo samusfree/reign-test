dd68249dcd2b669ed52c8d8357728b1fd0c17d98
Samuel Gonzales <sagonzales89@gmail.com>
2024-04-11 23:11:55 -0500

    feature initial spring project

---
+++ sgonzalesbetest/src/test/java/com/reign/sgonzales/betest/sgonzalesbetest/SgonzalesbetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reign.sgonzales.betest.sgonzalesbetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgonzalesbetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
